9bb79cb1b87ec70360dc3ea18346f8d5c613a525
Quentin Khoo <quentinkhoo@outlook.com>
2019-03-19 19:46:02 +0800

    Overlay pepe

--- app/src/main/java/com/example/overlayqrcodeapp/MainActivity.java
+++ app/src/main/java/com/example/overlayqrcodeapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.overlayqrcodeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +19,29 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onPause() {
         super.onPause();
-        for (int i =0; i < 100; i++) {
-            Toast.makeText(this, "Hehehehehee", Toast.LENGTH_LONG).show();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
         }
+        for (int i =0; i < 10; i++) {
+            doBackgroundToast();
+        }
+    }
+
+    private void checkForQRCode() {
+
+    }
+
+    private void doBackgroundToast() {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.qr_overlay_toast,
+                (ViewGroup) findViewById(R.id.qr_overlay));
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0,50);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
     }
 }
