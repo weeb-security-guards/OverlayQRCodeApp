e53b65a7db74adc4db9cc0fa14b16beb2279145e
Quentin Khoo <quentinkhoo@outlook.com>
2019-03-20 18:11:10 +0800

    Made attacker qr code

--- app/src/main/java/com/example/overlayqrcodeapp/MainActivity.java
+++ app/src/main/java/com/example/overlayqrcodeapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.overlayqrcodeapp;
 
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String DEBUG_TAG="Action: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +29,44 @@ public class MainActivity extends AppCompatActivity {
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         }
-        for (int i =0; i < 10; i++) {
+        for (int i =0; i < 5; i++) {
             doBackgroundToast();
         }
     }
 
-    private void checkForQRCode() {
-
+    @Override
+    public boolean onTouchEvent(MotionEvent  event) {
+        int action = MotionEventCompat.getActionMasked(event);
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN):
+                Log.d(DEBUG_TAG, "Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE):
+                Log.d(DEBUG_TAG, "Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP):
+                Log.d(DEBUG_TAG, "Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL):
+                Log.d(DEBUG_TAG, "Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE):
+                Log.d(DEBUG_TAG, "Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default:
+                return super.onTouchEvent(event);
+        }
     }
 
     private void doBackgroundToast() {
         LayoutInflater inflater = getLayoutInflater();
         View layout = inflater.inflate(R.layout.qr_overlay_toast,
-                (ViewGroup) findViewById(R.id.qr_overlay));
+                (ViewGroup) findViewById(R.id.qr_overlay_root));
         Toast toast = new Toast(getApplicationContext());
-        toast.setGravity(Gravity.CENTER, 0,50);
+        toast.setGravity(Gravity.CENTER, 0,0);
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
         toast.show();
-
     }
 }
